e0cd2ae874e1b3fd7a779df42aa9958b6e14b75e
abhishekgdev <abhishek.g.dev@gmail.com>
2016-01-19 11:52:26 +0530

    Made changes in the program

--- sample.c
+++ sample.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main() {
+	int num;
+
 	printf("\n Welcome... ");
+	
+	printf("\n Give a number : ");
+	scanf("%d", &num);
+
+	printf("\nNumber is : %d", num);
+
 	return 0;
 }
